c21fb7c08130d5847a5374e82c3bfbb5589ed900
Bruno007k <144683027+Bruno007k@users.noreply.github.com>
2023-09-25 09:06:09 -0300

    soma dos 5 numeros

---
+++ bruno_daniel.py
@@ -0,0 +1,8 @@
+n1 = int(input('digite um numero?'))
+n2 = int(input('digite um numero?'))
+n3 = int(input('digite um numero?'))
+n4 = int(input('digite um numero?'))
+n5 = int(input('digite um numero?'))
+#soma das variaveis anteriores
+m = n1+n2+n3+n4+n5
+print('o resultado é; ', m)
